cd09d677f3b01c0e34fa2564e791cb3fe393f8a4
ImalkaPerera <ImalkaPerera@users.noreply.github.com>
2026-02-06 03:20:12 +0530

    feat:Added dashboard route

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,8 +1,13 @@
 import {Router} from "express";
 import {register,login} from "../controllers/auth.controller.js";
+import { authenticateToken } from "../middleware/auth.middleware.js";
 
 const router=Router();
 
 router.post("/register",register);
 router.post("/login",login);
+
+router.get("/me", authenticateToken, (req,res)=>{
+    res.json({message:"This is a protected profile route",user:(req as any).user});
+});
 export default router;
